f87b6fafc9248f36344951bddb44bf79c59341bd
dbsd11 <bitdbsd11@163.com>
2023-12-23 12:10:55 +0800

    Create answer.go

---
+++ answer/answer.go
@@ -0,0 +1,176 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// pion-to-pion is an example of two pion instances communicating directly!
+package main
+
+import (
+        "bytes"
+        "encoding/json"
+        "flag"
+        "fmt"
+        "io"
+        "io/ioutil"
+        "net/http"
+        "os"
+        "sync"
+        "time"
+
+        "github.com/pion/webrtc/v4"
+        "github.com/pion/webrtc/v4/examples/internal/signal"
+)
+
+func main() { // nolint:gocognit
+        answerAddr := flag.String("answer-address", ":60000", "Address that the Answer HTTP server is hosted on.")
+        flag.Parse()
+
+        var candidatesMux sync.Mutex
+        pendingCandidates := make([]*webrtc.ICECandidate, 0)
+        // Everything below is the Pion WebRTC API! Thanks for using it ❤️.
+
+        // Prepare the configuration
+        config := webrtc.Configuration{
+                ICEServers: []webrtc.ICEServer{
+                        {
+                                URLs: []string{"stun:stun.l.google.com:19302"},
+                        },
+                        {
+                                URLs: []string{"turn:47.251.71.83:3478"},
+                                Username: "user-test",
+                                Credential: "123456",
+                                CredentialType: webrtc.ICECredentialTypePassword,
+                        },
+                },
+                ICETransportPolicy: webrtc.ICETransportPolicyRelay,
+                BundlePolicy:       webrtc.BundlePolicyBalanced,
+                RTCPMuxPolicy:      webrtc.RTCPMuxPolicyRequire,
+        }
+
+        // Create a new RTCPeerConnection
+        peerConnection, err := webrtc.NewPeerConnection(config)
+        if err != nil {
+                panic(err)
+        }
+        defer func() {
+                if err := peerConnection.Close(); err != nil {
+                        fmt.Printf("cannot close peerConnection: %v\n", err)
+                }
+        }()
+
+        // When an ICE candidate is available send to the other Pion instance
+        // the other Pion instance will add this candidate by calling AddICECandidate
+        peerConnection.OnICECandidate(func(c *webrtc.ICECandidate) {
+                if c == nil {
+                        return
+                }
+
+                fmt.Printf("ice candidate %v \n", *c);
+
+                candidatesMux.Lock()
+                defer candidatesMux.Unlock()
+
+                desc := peerConnection.RemoteDescription()
+                if desc != nil {
+                        pendingCandidates = append(pendingCandidates, c)
+                }
+        })
+
+        // A HTTP handler that processes a SessionDescription given to us from the other Pion process
+        http.HandleFunc("/sdp", func(w http.ResponseWriter, r *http.Request) {
+                sdp := webrtc.SessionDescription{}
+                if err := json.NewDecoder(r.Body).Decode(&sdp); err != nil {
+                        panic(err)
+                }
+
+                fmt.Printf("received offer %v\n", sdp)
+
+                if err := peerConnection.SetRemoteDescription(sdp); err != nil {
+                        panic(err)
+                }
+
+
+                // Create an answer to send to the other process
+                answer, err := peerConnection.CreateAnswer(nil)
+                if err != nil {
+                        panic(err)
+                }
+
+                peerConnection.SetLocalDescription(answer)
+
+                time.Sleep(1 * time.Second)
+
+                answer, err = peerConnection.CreateAnswer(nil)
+
+                result := make(map[string]interface{})
+
+                result["answer"] = answer
+                result["ices"] = pendingCandidates
+
+                fmt.Printf("return answer with ices %v\n", result)
+
+                // 将response数据对象编码为 JSON 格式
+                resDatas, _ := json.Marshal(result)
+
+                // 返回给浏览器
+                io.WriteString(w, string(resDatas))
+
+                candidatesMux.Lock()
+                for _, c := range pendingCandidates {
+                        onICECandidateErr := signalCandidate(*offerAddr, c)
+                        if onICECandidateErr != nil {
+                                //panic(onICECandidateErr)
+                        }
+                }
+                candidatesMux.Unlock()
+        })
+
+        // Set the handler for Peer connection state
+        // This will notify you when the peer has connected/disconnected
+        peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+                fmt.Printf("Peer Connection State has changed: %s\n", s.String())
+
+                if s == webrtc.PeerConnectionStateFailed {
+                        // Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+                        // Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+                        // Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+                        // fmt.Println("Peer Connection has gone to failed exiting")
+                        // os.Exit(0)
+                }
+
+                if s == webrtc.PeerConnectionStateClosed {
+                        // PeerConnection was explicitly closed. This usually happens from a DTLS CloseNotify
+                        fmt.Println("Peer Connection has gone to closed exiting")
+                        os.Exit(0)
+                }
+        })
+
+        // Register data channel creation handling
+        peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
+                fmt.Printf("New DataChannel %s %d\n", d.Label(), d.ID())
+
+                // Register channel opening handling
+                d.OnOpen(func() {
+                        fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label(), d.ID())
+
+                        for range time.NewTicker(5 * time.Second).C {
+                                message := signal.RandSeq(15)
+                                fmt.Printf("Sending '%s'\n", message)
+
+                                // Send the message as text
+                                sendTextErr := d.SendText(message)
+                                if sendTextErr != nil {
+                                        panic(sendTextErr)
+                                }
+                        }
+                })
+
+                // Register text message handling
+                d.OnMessage(func(msg webrtc.DataChannelMessage) {
+                        fmt.Printf("Message from DataChannel '%s': '%s'\n", d.Label(), string(msg.Data))
+                })
+        })
+
+        // Start HTTP server that accepts requests from the offer process to exchange SDP and Candidates
+        // nolint: gosec
+        panic(http.ListenAndServe(*answerAddr, nil))
+}
